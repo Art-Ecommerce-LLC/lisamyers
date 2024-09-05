713ea99bd049c90ec9254026e539baf596574408
myersben9 <120344142+myersben9@users.noreply.github.com>
2024-09-05 13:41:19 -0700

    static work

---
+++ frontend/my-loader.ts
@@ -0,0 +1,13 @@
+
+export default function cloudinaryLoader({
+  src,
+  width,
+  quality,
+}: {
+  src: string
+  width: number
+  quality?: number
+}) {
+  return `/${src}?w=${width}&q=${quality || 75}`;
+}
+    
